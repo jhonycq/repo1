26b94a6854155c8b0fdc4d0aeb2ae132f4f1778b
Jhony Quispe <jhony.quispe.contractor@bbva.com>
2024-01-12 20:10:35 -0500

    esta es un archivo scala

---
+++ variables.scala
@@ -0,0 +1,17 @@
+// COMENTARIOS
+/*
+ESTE ES UN COMENTARIO
+PARA VARIAS LINEAS DE 
+CODIGO
+*/
+
+// VARIABLES
+
+val Nombre = "Jhony"
+val Apellido_Paterno = "Quispe"
+val Apellido_Materno = "Cisneros"
+
+// CONDICONES-OPERADORES
+// OPERADORES RELACIONALES
+A == 5
+print(10==20)
